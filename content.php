79cebd29defd99c08183bab982f604e2eed0cca3
OmegaDanP <omega3ntertainment@gmail.com>
2016-01-21 13:21:05 +0000

    1
    1

---
+++ content.php
@@ -0,0 +1,77 @@
+<div id="wrap" class="container-fluid">
+    <div class="container-fluid jumby">
+        <span><a href="#">Omega Entertainment</a></span>
+    </div>
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Ω</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">About us</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contacts <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a class="members" href="#members">Members</a></li>
+                <li><a class="mail_us" href="#">Mail Us</a></li>
+              </ul>
+            </li>
+          </ul>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="mainContainer" class='container'>
+        <div id="carousel-example-generic" class="carousel slide container" data-ride="carousel" style="height:200px;">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="image/archeage.jpg" alt="...">
+                <div class="carousel-caption">
+                <h2>Coming Soon</h2>
+                </div>
+            </div>
+            <div class="item">
+                <img src="image/tera.png" alt="...">
+                <div class="carousel-caption">
+                    <h2>ArcheAge</h2>
+                </div>
+            </div>
+        </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div>
+        <div class="len_seperate"></div>
+        <div class="itemsload"></div>
+    </div>
+
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container">
+            <span class='footer'>
+                <a href="#"> © Omega Entertainment Network 2016</a>  <!-- OEN home -->
+                <a href="#">Site Developed by ♡Gluu♡</a> <!-- Gluu's page -->
+            </span>
+        </div>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ω Omega </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/lenny.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lenny.js"></script>
+  </head>
+  <body>
+    <?php
+
+    include("content.php");
+    include("footer.php");
+    ?>
